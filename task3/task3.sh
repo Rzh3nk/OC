050f37ce986395281ccb1ad7320c7043cf693c1a
Rzh3nk <147371569+Rzh3nk@users.noreply.github.com>
2024-05-19 14:57:30 +0000

    Update task3.sh

--- task3/task3.sh
+++ task3/task3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+res="result.txt"
+razr=".txt"
 
 if [ -z "$1" ]; then
     echo "Ошибка"
     exit 1
 fi
 searching="$1"
-res="result.txt"
-razr=".txt"
 find . -name "*${razr}" -type f -print0 | xargs -0 grep -l "${searching}" >> "${res}"
